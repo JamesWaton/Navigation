e116a22c34fbbf0302ce08a42dcc06f36ff8a735
james watson <jameswatson98@hotmail.com>
2021-01-01 12:31:11 +1100

    Naviagtion

--- App.js
+++ App.js
@@ -1,21 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+function Profile() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{ flex: 1, justifyContent: 'space-between', alignItems: 'center' }}
+    >
+      <Text>This is top text.</Text>
+      <Text>This is bottom text.</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function  Home() {
+  return (
+    <View
+      style={{ flex: 1, justifyContent: 'space-between', alignItems: 'center' }}
+    >
+      <Text>This is top text.</Text>
+      <Text>This is bottom text.</Text>
+    </View>
+  );
+}
+function Settings() {
+  return (
+    <View
+      style={{ flex: 1, justifyContent: 'space-between', alignItems: 'center' }}
+    >
+      <Text>This is top text.</Text>
+      <Text>This is bottom text.</Text>
+    </View>
+  );
+}
+function Notifications() {
+  return (
+    <View
+      style={{ flex: 1, justifyContent: 'space-between', alignItems: 'center' }}
+    >
+      <Text>This is top text.</Text>
+      <Text>This is bottom text.</Text>
+    </View>
+  );
+}
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+
+
+function HomeStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={App} />
+      <Stack.Screen name="Profile" component={Profile} />
+      <Stack.Screen name="Settings" component={Settings} />
+    </Stack.Navigator>
+  );
+}
+
+function App() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeStack} />
+      <Tab.Screen name="Feed" component={Home} />
+      <Tab.Screen name="Notifications" component={Notifications} />
+    </Tab.Navigator>
+  );
+}
